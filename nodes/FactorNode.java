00229dd35dbefc66ea9fb6e6a707aba06e6e8524
Biello <biello94@live.it>
2018-07-08 12:10:17 +0200

    updated FactorNode: aggiustati i controlli del typeCheck relativi all' EQ

--- nodes/FactorNode.java
+++ nodes/FactorNode.java
@@ -1,8 +1,6 @@
 package nodes;
 
-import type.BoolType;
-import type.IType;
-import type.IntType;
+import type.*;
 import exceptions.TypeException;
 import parser.FOOLParser.FactorContext;
 import symboltable.SymbolTable;
@@ -44,15 +42,38 @@ public class FactorNode implements INode {
         IType leftType = leftNode.typeCheck();
         IType rightType = rightNode.typeCheck();
 
+
+/*        if (leftNode instanceof FunCallNode || rightNode instanceof FunCallNode) {// TODO non è possibile inserire delle funzioni come condizione
+            throw new TypeException("Tipo incompatibile per " + operator + ". Non è possbile inserire delle funzioni come condizione", factorContext);
+        }*/
+
+/*        if (leftType instanceof ObjectType || rightType instanceof ObjectType) {// TODO non è possibile inserire degli oggetti come condizione
+            throw new TypeException("Tipo incompatibile per " + operator + ". Non è possbile inserire degli Oggetti nella condizione", factorContext);
+        }*/
+
+
         if (operator.equals("And") || (operator.equals("Or"))) {
             if (!leftType.isSubType(new BoolType()) || !rightType.isSubType(new BoolType())) {
                 throw new TypeException("Tipo incompatibile per " + operator + ". È richiesto un booleano.", factorContext);
+            }
+        } else if (operator.equals("Eq")) {
+            if (!leftType.isSubType(rightType)) {
+                if (!(leftType instanceof ObjectType) && !(rightType instanceof ObjectType)) {
+                    throw new TypeException("Tipo incompatibile per " + operator + ". È richiesto il medesimo tipo (int or bool).\nTipi passati: (op1: " + leftType.toPrint() + " op2: " + rightType.toPrint() + ")", factorContext);
+                } else if (leftType instanceof ObjectType && rightType instanceof ObjectType) {
+                    throw new TypeException("Tipo incompatibile per " + operator + ". È richiesto che l'oggetto sia del medesimo tipo.\nTipi passati: (op1: " + leftType.toPrint() + " op2: " + rightType.toPrint() + ")", factorContext);
+                } else {
+                    throw new TypeException("Tipo incompatibile per " + operator + ". È richiesto il medesimo tipo (ClassType or not ClassType).\nTipi passati: (op1: " + leftType.toPrint() + " op2: " + rightType.toPrint() + ")", factorContext);
+                }
             }
         } else {
             if (!leftType.isSubType(new IntType()) || !rightType.isSubType(new IntType())) {
                 throw new TypeException("Tipo incompatibile per " + operator + ". È richiesto un intero.", factorContext);
             }
         }
+
+
+        System.out.println("\nTipi compatibili per '"+operator+"'. (op1 = " + leftType.toPrint() + " op2 = " + rightType.toPrint() + ")\n"); // TODO al momento vengono accettati i seguenti tipi: int, bool, ID
 
         return new BoolType();
     }
